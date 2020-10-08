a90c1d1141513c566a2d81b78bcc21c7f0fa3bf9
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-10-08 13:10:25 +0300

    Hello World

---
+++ 001-Java-Basics/001_helloWorld/src/helloWorld/main.java
@@ -0,0 +1,14 @@
+package helloWorld;
+/*
+ * Simple hello world program
+ * 
+ * */
+public class main {
+
+	public static void main(String[] args) {
+		// Prints given string to console
+		System.out.println("Hello World!!");
+
+	}
+
+}
