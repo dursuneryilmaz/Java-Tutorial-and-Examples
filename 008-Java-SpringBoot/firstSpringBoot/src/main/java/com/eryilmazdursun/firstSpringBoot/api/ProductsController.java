10ef793836a76642e9437b8793be2a1e6f03b99c
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-19 20:04:05 +0300

    first rest controller created

---
+++ 008-Java-SpringBoot/firstSpringBoot/src/main/java/com/eryilmazdursun/firstSpringBoot/api/ProductsController.java
@@ -0,0 +1,12 @@
+package com.eryilmazdursun.firstSpringBoot.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+	@GetMapping("/")
+	public String get() {
+		return "Laptop";
+	}
+}
