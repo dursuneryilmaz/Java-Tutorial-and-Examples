7df9ad83c119ef6fd61efe5913a96b520acc8ba3
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-21 20:50:03 +0300

    Initial commit

---
+++ 008-Java-SpringBoot/hibernateJpaRestApi/src/main/java/com/dursuneryilmaz/hibernateJpaRestApi/HibernateJpaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.hibernateJpaRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaRestApiApplication.class, args);
+	}
+
+}
---
+++ 008-Java-SpringBoot/hibernateJpaRestApi/src/test/java/com/dursuneryilmaz/hibernateJpaRestApi/HibernateJpaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.hibernateJpaRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
