acb61ad68b4a47cec148199b6d1729252bf74ab8
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-27 21:36:45 +0300

    user repositor and creteUser service method

---
+++ 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dursuneryilmaz.mobileappws;
+
+import com.dursuneryilmaz.mobileappws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
--- 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/service/impl/UserService.java
+++ 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/service/impl/UserService.java
@@ -1,13 +1,30 @@
 package com.dursuneryilmaz.mobileappws.service.impl;
 
+import com.dursuneryilmaz.mobileappws.UserRepository;
+import com.dursuneryilmaz.mobileappws.io.entity.UserEntity;
 import com.dursuneryilmaz.mobileappws.service.IUserService;
 import com.dursuneryilmaz.mobileappws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService {
+    @Autowired
+    UserRepository userRepository;
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        //hardcode required fields for test
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("test");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+        UserDto returnedValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnedValue);
+
+        return returnedValue;
     }
 }
