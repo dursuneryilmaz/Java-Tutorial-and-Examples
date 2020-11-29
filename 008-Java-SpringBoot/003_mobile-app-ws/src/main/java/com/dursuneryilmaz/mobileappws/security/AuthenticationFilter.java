5e18fcb92c40d64aa46e86203a4e81f2bb7dc9c8
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-29 23:34:17 +0300

    reading token secret from property file

--- 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/security/AuthenticationFilter.java
+++ 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/security/AuthenticationFilter.java
@@ -55,7 +55,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         String token = Jwts.builder()
                 .setSubject(userName)
                 .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret())
                 .compact();
         // it can be Autowired or not ?
         IUserService userService = (IUserService) SpringApplicationContext.getBean("userService");
