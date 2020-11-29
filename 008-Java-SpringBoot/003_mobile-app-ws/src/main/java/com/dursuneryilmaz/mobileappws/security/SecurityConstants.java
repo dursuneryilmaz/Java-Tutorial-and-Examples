8a8efed118f4de76d024e1df9603e544ab265dd4
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-29 21:55:22 +0300

    idea commit test

--- 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/security/SecurityConstants.java
+++ 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.dursuneryilmaz.mobileappws.security;
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 864000000;
+    public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
