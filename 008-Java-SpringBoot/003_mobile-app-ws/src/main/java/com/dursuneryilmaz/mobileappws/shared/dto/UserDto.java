673bf5626560f636da2f89f46d4ae3525cb23449
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-03 16:11:36 +0300

    AddressDto created and link with UserDto

--- 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/shared/dto/UserDto.java
+++ 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/shared/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.dursuneryilmaz.mobileappws.shared.dto;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class UserDto implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -13,6 +14,8 @@ public class UserDto implements Serializable {
     private String encryptedPassword;
     private String emailVerificationToken;
     private Boolean emailVerificationStatus = false;
+
+    private List<AddressDto> addresses;
 
     public long getId() {
         return id;
@@ -84,5 +87,13 @@ public class UserDto implements Serializable {
 
     public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
         this.emailVerificationStatus = emailVerificationStatus;
+    }
+
+    public List<AddressDto> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<AddressDto> addresses) {
+        this.addresses = addresses;
     }
 }
