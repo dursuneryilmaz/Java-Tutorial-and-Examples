eb6a0bc9f786c578614a4a1efe222329c347b724
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-05 14:14:57 +0300

    Update UserController.java

--- 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/ui/controller/UserController.java
+++ 008-Java-SpringBoot/003_mobile-app-ws/src/main/java/com/dursuneryilmaz/mobileappws/ui/controller/UserController.java
@@ -11,10 +11,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Type;
