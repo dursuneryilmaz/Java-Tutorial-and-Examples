fc69135e4b3f265d5df360d135730cf3bfc88a65
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-08 00:49:24 +0300

    password reset request and reset models
    catch the request body and map to these model to use

--- 008-Java-SpringBoot/003_user-service-api/src/main/java/com/dursuneryilmaz/mobileappws/ui/model/response/RequestOperationName.java
+++ 008-Java-SpringBoot/003_user-service-api/src/main/java/com/dursuneryilmaz/mobileappws/ui/model/response/RequestOperationName.java
@@ -3,5 +3,5 @@ package com.dursuneryilmaz.mobileappws.ui.model.response;
 import javax.persistence.AssociationOverride;
 
 public enum RequestOperationName {
-    DELETE, VERIFY_EMAIL
+    DELETE, VERIFY_EMAIL, PASSWORD_RESET_REQUEST, PASSWORD_RESET
 }
