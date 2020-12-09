a89090f4f0907278a4a8b80c32d856b5853ee115
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-09 23:50:43 +0300

    Create UserServiceTest.java
    simple jUnit 5 tests. assertNotNull and assetEquals

---
+++ 008-Java-SpringBoot/003_user-service-api/src/test/java/com/dursuneryilmaz/mobileappws/service/impl/UserServiceTest.java
@@ -0,0 +1,48 @@
+package com.dursuneryilmaz.mobileappws.service.impl;
+
+import com.dursuneryilmaz.mobileappws.io.entity.UserEntity;
+import com.dursuneryilmaz.mobileappws.io.repository.IUserRepository;
+import com.dursuneryilmaz.mobileappws.service.IUserService;
+import com.dursuneryilmaz.mobileappws.shared.dto.UserDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    // this cannot be mocked it must be real object, this provided @InjectMocks annotation.
+    @InjectMocks
+    UserService userService;
+    // mock object to be injected
+    @Mock
+    IUserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        // for mockito be able to instantiate objects
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getUserByEmail() {
+        // Step object for to return on repository findByEmail method
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setFirstName("Dursun");
+        userEntity.setUserId("asf43t53yhtbd");
+        userEntity.setEncryptedPassword("afdgeg457547474");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
+        UserDto userDto = userService.getUserByEmail("test@test.com");
+
+        // test object is not null
+        assertNotNull(userDto);
+        // test bean utils works on bussiness logic side
+        assertEquals("Dursun", userDto.getFirstName());
+    }
+}
