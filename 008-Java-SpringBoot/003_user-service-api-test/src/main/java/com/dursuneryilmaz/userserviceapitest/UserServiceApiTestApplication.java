38ca08a67a47100e3fd84d29c550ec338499beae
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-12 18:14:49 +0300

    initial commit
    rest assured tests for 003_user-service-api project

---
+++ 008-Java-SpringBoot/003_user-service-api-test/src/main/java/com/dursuneryilmaz/userserviceapitest/UserServiceApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.userserviceapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApiTestApplication.class, args);
+    }
+
+}
---
+++ 008-Java-SpringBoot/003_user-service-api-test/src/test/java/com/dursuneryilmaz/userserviceapitest/UserServiceApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.userserviceapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
